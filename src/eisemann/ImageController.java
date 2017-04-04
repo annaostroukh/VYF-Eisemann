1fb2ca79f3987c45685c56fd604d16b291c5c264
Anna Ostroukh <annaostroukh@gmail.com>
2017-04-04 09:17:17 +0200

    Refactoring, white balance implementation

--- src/eisemann/ImageController.java
+++ src/eisemann/ImageController.java
@@ -4,6 +4,7 @@ import java.io.ByteArrayInputStream;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.Iterator;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -11,8 +12,10 @@ import org.opencv.core.Core;
 import org.opencv.core.CvType;
 import org.opencv.core.Mat;
 import org.opencv.core.MatOfByte;
+import org.opencv.core.Scalar;
 import org.opencv.imgcodecs.Imgcodecs;
 import org.opencv.imgproc.Imgproc;
+import org.opencv.utils.Converters;
 
 import javafx.scene.image.Image;
 
@@ -74,79 +77,124 @@ public class ImageController {
      * @return 
     */
 	public void getFinal() {
-		//Mat intensity = computeIntensity(getImageFlash());
-		Mat largeScale = computeLargeScale(getImageNoFlash());
-		Mat detail = computeDetail(getImageFlash());
-		Mat color = computeColor(getImageFlash());
+		imgNoFlash = Imgcodecs.imread(getImageNoFlash());
+		imgFlash = Imgcodecs.imread(getImageFlash());
+		Mat intensityF = computeIntensity(imgFlash);
+		Mat intensityNF = computeIntensity(imgNoFlash);
+		Mat largeScaleNF = computeLargeScale(intensityNF);
+		Mat largeScaleF = computeLargeScale(intensityF);
+		Mat detail = computeDetail(intensityF, largeScaleF);
+		Mat color = computeColor(imgFlash, intensityF);
 		Mat finalR = new Mat();
 		Mat finalG = new Mat();
 		Mat finalB = new Mat();
 		Mat image = new Mat();
+		Mat weightsWB = new Mat();
+		List<Double> weights = new ArrayList();
 		
 		color.convertTo(color, CvType.CV_64F);
-		largeScale.convertTo(largeScale, CvType.CV_64F);
+		largeScaleNF.convertTo(largeScaleNF, CvType.CV_64F);
 		detail.convertTo(detail, CvType.CV_64F);
 		
-		Core.add(detail, largeScale, imgNoFlash);
+		Core.add(detail, largeScaleNF, image);
 		
-		List<Mat> rgb = new ArrayList<Mat>(3);
-		Core.split(color, rgb);
-		Mat R = rgb.get(0);
-		Mat G = rgb.get(1);
-		Mat B = rgb.get(2);
+		List<Mat> rgb = getRGB(color);
 	
-		Core.add(imgNoFlash, R, finalR);
-		Core.add(imgNoFlash, G, finalG);
-		Core.add(imgNoFlash, B, finalB);
+		Core.add(image, rgb.get(0), finalR);
+		Core.add(image, rgb.get(1), finalG);
+		Core.add(image, rgb.get(2), finalB);
+		
+		weights = whiteBalanceCorr(imgFlash, imgNoFlash);
+		Mat wb = weightsWB.diag(0);
+		wb.put(0, 0, weights.get(0));
+		wb.put(1, 1, weights.get(1));
+		wb.put(2, 2, weights.get(2));
 
 		List<Mat> finalRGB = new ArrayList<Mat>(3);
 		finalRGB.add(0,finalR);
 		finalRGB.add(1,finalG);
 		finalRGB.add(2,finalB);
 		Core.merge(finalRGB, image);
+		
+		System.out.println(image.cols() + " " + image.rows());
+		System.out.println(wb.cols() + " " + wb.rows());
+		
+		Core.multiply(wb, image, image);
 
 		Imgcodecs.imencode(".jpg", bcNormalize(image), buffer);
 		imageResult = new Image(new ByteArrayInputStream(buffer.toArray()));
 		setImageResult(imageResult);
 		
 		// Layers test
-		Imgcodecs.imencode(".jpg", color, buffer);
+		Imgcodecs.imencode(".jpg", intensityF, buffer);
 		imageResultTest = new Image(new ByteArrayInputStream(buffer.toArray()));
 		setImageResultTest(imageResultTest);	
 	}
 	
 	/**
+     * Method computes weighted values for white balance correction
+     * @param image with flash, image without flash
+     * @return weighted values
+    */
+	private List<Double> whiteBalanceCorr(Mat imgF, Mat imgNF) {
+		imgF.convertTo(imgF, CvType.CV_64F);
+		imgNF.convertTo(imgNF, CvType.CV_64F);
+		Mat output = new Mat();
+		double weightR = 0.0;
+		double weightG = 0.0;
+		double weightB = 0.0;
+		List<Double> weights = new ArrayList(3);
+		List<Mat> imgFRGB = getRGB(imgF);
+		List<Mat> imgNFRGB = getRGB(imgNF);
+		
+		weightR = (getSum(getBrightPixels(imgFRGB.get(0), getMax(imgFRGB.get(0)) - 20)) 
+				+ getSum(getBrightPixels(imgNFRGB.get(0), getMax(imgFRGB.get(0)) - 20))) 
+				/ getBrightPixels(imgFRGB.get(0), getMax(imgFRGB.get(0)) - 20).size() 
+				+ getBrightPixels(imgNFRGB.get(0), getMax(imgFRGB.get(0)) - 20).size();
+		weightG = (getSum(getBrightPixels(imgFRGB.get(1), getMax(imgFRGB.get(1)) - 10)) 
+				+ getSum(getBrightPixels(imgNFRGB.get(1), getMax(imgFRGB.get(1)) - 10))) 
+				/ getBrightPixels(imgFRGB.get(1), getMax(imgFRGB.get(1)) - 10).size() 
+				+ getBrightPixels(imgNFRGB.get(1), getMax(imgFRGB.get(1)) - 10).size();
+		weightB = (getSum(getBrightPixels(imgFRGB.get(2), getMax(imgFRGB.get(2)))) 
+				+ getSum(getBrightPixels(imgNFRGB.get(2), getMax(imgFRGB.get(2))))) 
+				/ getBrightPixels(imgFRGB.get(2), getMax(imgFRGB.get(2))).size() 
+				+ getBrightPixels(imgNFRGB.get(2), getMax(imgFRGB.get(2))).size();
+		
+		System.out.println(weightR);
+		System.out.println(weightG);
+		System.out.println(weightB);
+		
+		weights.add(weightR);
+		weights.add(weightG);
+		weights.add(weightB);
+		return weights;
+	}
+		
+	/**
      * Method computes the detail layer of image (intensity divided by large scale layer)
-     * @param path to image
+     * @param intensity level, large scale level
      * @return detail layer
     */
-	private Mat computeDetail(String imagePath) {
+	private Mat computeDetail(Mat intensity, Mat largeScale) {
+		intensity.convertTo(intensity, CvType.CV_32F);
 		Mat detail = new Mat();
-		Mat intensity = new Mat();
-		Mat largeScale = new Mat();
-		
-		intensity = computeIntensity(imagePath);
-		largeScale = computeLargeScale(imagePath);
-		largeScale.convertTo(largeScale, CvType.CV_64F);
 		
 		Core.divide(intensity, largeScale, detail, 255.0);
-
+	
 		return bcNormalize(detail);
 	}
 	
 	/**
      * Method computes the large scale layer of image (using bilateral filtering on intensity)
-     * @param path to image
+     * @param intensity level
      * @return large scale
     */
-	private Mat computeLargeScale(String imagePath) {
-		Mat intensity = computeIntensity(imagePath);
+	private Mat computeLargeScale(Mat intensity) {
+		intensity.convertTo(intensity, CvType.CV_32F);
 		Mat output = new Mat();
 		double sigmaColor = 40;
 		double sigmaSpace = 40;
  
-		intensity.convertTo(intensity, CvType.CV_32F);
-		
 		Imgproc.bilateralFilter(intensity, output, 15, sigmaColor, sigmaSpace);
 		
 		return output;
@@ -154,29 +202,21 @@ public class ImageController {
 	
 	/**
      * Method computes the color layer of image (original image divided by intensity)
-     * @param path to image
+     * @param image matrix, intensity level
      * @return color
     */
-	private Mat computeColor(String imagePath) {
+	private Mat computeColor(Mat img, Mat intensity) {
+		img.convertTo(img, CvType.CV_64FC3);
 		Mat color = new Mat();
 		Mat colorR = new Mat();
 		Mat colorG = new Mat();
 		Mat colorB = new Mat();
-		Mat originalImage = new Mat();
-		Mat intensity = computeIntensity(imagePath);
-
-		originalImage = Imgcodecs.imread(imagePath);
-		originalImage.convertTo(originalImage, CvType.CV_64FC3);
 		
-		List<Mat> rgb = new ArrayList<Mat>(3);
-		Core.split(originalImage, rgb);
-		Mat R = rgb.get(0);
-		Mat G = rgb.get(1);
-		Mat B = rgb.get(2);
+		List<Mat> rgb = getRGB(img);
 		
-		Core.divide(R, intensity, colorR, 255.0, CvType.CV_8UC1);
-		Core.divide(G, intensity, colorG, 255.0, CvType.CV_8UC1);
-		Core.divide(B, intensity, colorB, 255.0, CvType.CV_8UC1);
+		Core.divide(rgb.get(0), intensity, colorR, 255.0, CvType.CV_8UC1);
+		Core.divide(rgb.get(1), intensity, colorG, 255.0, CvType.CV_8UC1);
+		Core.divide(rgb.get(2), intensity, colorB, 255.0, CvType.CV_8UC1);
 		
 		List<Mat> colorRGB = new ArrayList<Mat>(3);
 		colorRGB.add(0,colorR);
@@ -189,11 +229,11 @@ public class ImageController {
 	
 	/**
      * Method computes the intensity layer of image
-     * @param path to image 
+     * @param image matrix
      * @return intensity
     */
-	private Mat computeIntensity(String imagePath) {
-		Mat imgFinal = new Mat();
+	private Mat computeIntensity(Mat img) {
+		img.convertTo(img, CvType.CV_64FC3);
 		Mat sum = new Mat();
 		Mat divR = new Mat();
 		Mat divG = new Mat();
@@ -203,28 +243,21 @@ public class ImageController {
 		Mat intensB = new Mat();
 		Mat intensity = new Mat();
 		
-		imgFinal = Imgcodecs.imread(imagePath);
-		imgFinal.convertTo(imgFinal, CvType.CV_64FC3);
-		
-		List<Mat> rgb = new ArrayList<Mat>(3);
-		Core.split(imgFinal, rgb);
-		Mat R = rgb.get(0);
-		Mat G = rgb.get(1);
-		Mat B = rgb.get(2);
+		List<Mat> rgb = getRGB(img);
 				
-		Core.add(R, G, sum);
-		Core.add(sum, B, sum);
+		Core.add(rgb.get(0), rgb.get(1), sum);
+		Core.add(sum, rgb.get(2), sum);
 				
 		// Getting intensity according to formula in the article I = (R/sum(R,G,B))*R + (G/sum(R,G,B))*G + (B/sum(R,G,B))*B
-		Core.divide(R, sum, divR, 255.0);
-		Core.divide(G, sum, divG, 255.0);
-		Core.divide(B, sum, divB, 255.0);
+		Core.divide(rgb.get(0), sum, divR);
+		Core.divide(rgb.get(1), sum, divG);
+		Core.divide(rgb.get(2), sum, divB);
 
-		Core.multiply(divR, R, intensR);
-		Core.multiply(divG, G, intensG);
-		Core.multiply(divB, B, intensB);
+		Core.multiply(divR, rgb.get(0), intensR);
+		Core.multiply(divG, rgb.get(1), intensG);
+		Core.multiply(divB, rgb.get(2), intensB);
 				
-		Core.add(intensR, intensB, intensity);
+		Core.add(intensR, intensG, intensity);
 		Core.add(intensity, intensB, intensity);
 		
 		return bcNormalize(intensity);
@@ -236,15 +269,9 @@ public class ImageController {
      * @return normalized image
     */
 	private Mat bcNormalize(Mat img) {
-		img.convertTo(img, CvType.CV_64F);
 		
-		int size = (int) (img.total() * img.channels());
-		double[] temp = new double[size];
-		img.get(0, 0, temp);
-		
-		List<Double> pixels = Arrays.stream(temp).boxed().collect(Collectors.toList());
-		Object minValue = Collections.min(pixels).intValue();
-		Object maxValue = Collections.max(pixels).intValue();
+		Object minValue = Collections.min(Mat2Double(img)).intValue();
+		Object maxValue = Collections.max(Mat2Double(img)).intValue();
 
 		int range = (int) maxValue - (int)minValue;
 		float alpha = (float) 255 / range;
@@ -254,6 +281,81 @@ public class ImageController {
 		
 		return img;
 	}
+	
+	/**
+     * Method computes bright pixels for WB correstion
+     * @param channel matrix, bright pixel value boundary
+     * @return list of bright pixels
+    */
+	private List<Double> getBrightPixels(Mat channel, double brPixel) {
+		List<Double> brPixels = new ArrayList();
+		List<Double> ch = Mat2Double(channel);
+		
+		Iterator<Double> iterator = ch.iterator();
+		while (iterator.hasNext()) {
+			if (iterator.next() >= brPixel) {
+				brPixels.add(iterator.next());
+			}
+		}
+		//System.out.println(brPixels.size());
+		return brPixels;
+	}
+	
+	/**
+     * Method converts Mat do List of Double
+     * @param image to convert
+     * @return list
+    */
+	private List<Double> Mat2Double (Mat img) {
+		img.convertTo(img, CvType.CV_64F);
+		
+		int size = (int) (img.total() * img.channels());
+		double[] temp = new double[size];
+		img.get(0, 0, temp);
+		List<Double> pixels = Arrays.stream(temp).boxed().collect(Collectors.toList());
+		
+		return pixels;
+	}
+	
+	/**
+     * Method for splitting image into RGB channels
+     * @param image to split 
+     * @return RGB channels list
+    */
+	private List<Mat> getRGB (Mat img) {
+		img.convertTo(img, CvType.CV_64F);
+		
+		List<Mat> imgRGB = new ArrayList<Mat>(3);
+		Core.split(img, imgRGB);
+		
+		return imgRGB;
+	}
+	
+	/**
+     * Method returns maximum value from image matrix
+     * @param image matrix 
+     * @return maximum value
+    */
+	private double getMax(Mat matrix) {
+		double max = 0.0;
+		max = Collections.max(Mat2Double(matrix)).intValue();
+		return max;
+	}
+	
+	/**
+     * Method for finding sum of all elements in list
+     * @param list of elements 
+     * @return sum of elements
+    */
+	private double getSum(List<Double> list) {
+		double sum = 0.0;
+		Iterator<Double> iterator = list.iterator();
+		while (iterator.hasNext()) {
+			sum =+ iterator.next();
+		}
+		return sum;
+	}
+
 		
 
 }
